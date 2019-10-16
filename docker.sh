949564ca4069081e406c7a5a2ccce0f0148a593f
Willi Martens <w.martens@mehrwerk.de>
2019-10-16 11:01:07 +0200

    sort docker aliases / adds docker stop alias

--- docker.sh
+++ docker.sh
@@ -1,12 +1,12 @@
 alias dr="docker"
 alias dr.e="docker exec -it"
 alias dr.i="docker images"
-alias dr.r="docker run"
 alias dr.l="docker logs"
+alias dr.ps="docker ps"
+alias dr.ps:a="docker ps -a"
+alias dr.sp="docker stop"
+alias dr.r="docker run"
 alias dr.rm="docker rm"
 alias dr.rm.all='dr-stop-all; docker rm $(docker ps -a -q)'
 alias dr.rmi="docker rmi"
-alias dr.rmi.untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
-alias dr.ps="docker ps"
-alias dr.ps:a="docker ps -a"
-alias dr.sp.all='docker stop $(docker ps -q)'
+alias dr.rmi.untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'alias dr.sp.all='docker stop $(docker ps -q)'
