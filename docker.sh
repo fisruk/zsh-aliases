57a6954810a8a96aff180487604c25c2a462c425
Willi Martens <w.martens@mehrwerk.de>
2020-02-13 17:24:39 +0100

    Refactor docker ps command - adds docker ps filters

--- docker.sh
+++ docker.sh
@@ -3,6 +3,9 @@ alias dr.e="docker exec -it"
 alias dr.i="docker images"
 alias dr.l="docker logs"
 alias dr.ps='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}"'
+alias dr.ps.service='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^service*"'
+alias dr.ps.portal='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^portal*"'
+alias dr.ps.sync='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^sync-*"'
 alias dr.ps:a="docker ps -a"
 alias dr.sp="docker stop"
 alias dr.r="docker run"
