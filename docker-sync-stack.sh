7c6a1ac24774868a66da30b790c8486c77462381
Willi Martens <w.martens@mehrwerk.de>
2020-02-17 12:11:31 +0100

    Refactoring docker-sync-stack aliases

--- docker-sync-stack.sh
+++ docker-sync-stack.sh
@@ -1,7 +1,7 @@
 #--- DOCKER SYNC / SYNC-STACK ---#
-alias dr.s.start='docker-sync start'
-alias dr.s-sync='docker-sync sync'
-alias dr.s.list='docker-sync list'
-alias dr.s.clean='docker-sync clean'
-alias dr.ss.start='docker-sync-stack start'
-alias dr.ss.clean='docker-sync-stack clean'
+alias dr.s.s='docker-sync start'
+alias dr.s.sync='docker-sync sync'
+alias dr.s.l='docker-sync list'
+alias dr.s.c='docker-sync clean'
+alias dr.ss.st='docker-sync-stack start'
+alias dr.ss.c='docker-sync-stack clean'
