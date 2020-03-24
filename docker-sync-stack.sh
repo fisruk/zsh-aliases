73c209ada6153e8564e46197c389b7aae7e56ee2
Willi Martens <w.martens@mehrwerk.de>
2020-03-24 11:04:45 +0100

    Docker: Sortierung bei ps nach Namen

--- docker-sync-stack.sh
+++ docker-sync-stack.sh
@@ -1,7 +1,7 @@
 #--- DOCKER SYNC / SYNC-STACK ---#
-alias ds.s='docker-sync start'
+alias ds.s='docker-sync clean; docker-sync start'
 alias ds.sync='docker-sync sync'
 alias ds.l='docker-sync list'
 alias ds.c='docker-sync clean'
-alias dss.st='docker-sync-stack start'
+alias dss.st='docker-sync-stack clean; docker-sync-stack start'
 alias dss.c='docker-sync-stack clean'
