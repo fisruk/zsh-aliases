93efd6d8c976b3a26ca57f11e113ef92337d837e
Willi Martens <wm@martens-it.de>
2020-04-26 22:07:31 +0200

    Aliases eingekürzt

--- docker.sh
+++ docker.sh
@@ -1,15 +1,15 @@
-alias dr="docker"
-alias dr.e="docker exec -it"
-alias dr.i="docker images"
-alias dr.l="docker logs"
-alias dr.ps='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}"'
-alias dr.ps.service='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^service*" | sort'
-alias dr.ps.portal='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^portal*" | sort'
-alias dr.ps.sync='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^sync-*" | sort'
-alias dr.ps:a="docker ps -a"
-alias dr.sp="docker stop"
-alias dr.r="docker run"
-alias dr.rm="docker rm"
-alias dr.rm.all='dr-stop-all; docker rm $(docker ps -a -q)'
-alias dr.rmi="docker rmi"
-alias dr.rmi.untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'alias dr.sp.all='docker stop $(docker ps -q)'
+alias d="docker"
+alias d.e="docker exec -it"
+alias d.i="docker images"
+alias d.l="docker logs"
+alias d.ps='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}"'
+alias d.ps.service='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^service*" | sort'
+alias d.ps.portal='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^portal*" | sort'
+alias d.ps.sync='docker ps --format "table {{.Names}}\t{{.Ports}}\t{{.ID}}" --filter "name=^sync-*" | sort'
+alias d.ps:a="docker ps -a"
+alias d.sp="docker stop"
+alias d.r="docker run"
+alias d.rm="docker rm"
+alias d.rm.all='d-stop-all; docker rm $(docker ps -a -q)'
+alias d.rmi="docker rmi"
+alias d.rmi.untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'alias d.sp.all='docker stop $(docker ps -q)'
